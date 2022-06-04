2779f44e4bc6d82d0e4b33a76ac14845d64e3811
categerjsy <katerinagerak99@gmail.com>
2022-06-04 19:31:38 +0300

    Pleas work

--- partTh.php
+++ partTh.php
@@ -3,14 +3,14 @@
 //$_SESSION['URL']
 $id_hunt=$_SESSION["part_id"];
 
-unset($_SESSION["part_id"]);
 
 $team=$_POST["team"];
 
-$sql= "INSERT INTO participate (id_thunt,id_team)
+$sql= "INSERT INTO participate(id_thunt,id_team)
     VALUES ('$id_hunt','$team')";
 mysqli_query($conn,$sql);
 
+unset($_SESSION["part_id"]);
 header("location:homepage.php?msg=huntpart"); 
 
 
