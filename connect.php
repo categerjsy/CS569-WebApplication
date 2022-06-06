23310f67b6b6008765cef27187ed63f34d63e6ee
categerjsy <katerinagerak99@gmail.com>
2022-06-06 11:05:46 +0300

    check age v9

--- connect.php
+++ connect.php
@@ -55,7 +55,7 @@ session_start ();
 				$id = mysqli_query($conn,"select id_user from user where password='$password' AND username='$username'");
 				 while ($row = mysqli_fetch_array($id, MYSQLI_ASSOC)) {
 					$my_user=$row["id_user"];
-                    $dateOfBirth = date('Y-m-d',$row["birth"]);
+                    $dateOfBirth = strtotime($row["birth"]);
                     $today = date("Y-m-d");
                     $diff = date_diff(date_create($dateOfBirth), date_create($today));
                     $_SESSION["age"]=$diff->format('%y');
