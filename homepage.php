95368c77f04598b244ab18d8c611e947cba0ea5d
categerjsy <katerinagerak99@gmail.com>
2022-06-06 10:53:50 +0300

    check age v6

--- homepage.php
+++ homepage.php
@@ -55,13 +55,11 @@ session_start ();
       <div class="about-wrapper container">
         <div class="about-text">
           <p class="small">Treasure Hunt</p>
-          
+          <h2>Build the ultimate treasure hunt!</h2>
           <?php 
           $diff=$_SESSION["age"];
-          echo 'Your age is '.$diff;
+          echo $diff;
           ?>
-
-          <h2>Build the ultimate treasure hunt!</h2>
           <p>
             1. Plan your route
           </p>
