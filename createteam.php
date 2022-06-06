06cea0c4a819d1aef53096d61e2d2bc23a9b3f49
categerjsy <katerinagerak99@gmail.com>
2022-06-06 12:22:55 +0300

    team already exist scripts

--- createteam.php
+++ createteam.php
@@ -108,4 +108,7 @@ session_start ();
     </script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="js/number.js"></script>
+  <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
 </html>
