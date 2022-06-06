227577efb90579144c7316ac12200f0a6a29c0ee
categerjsy <katerinagerak99@gmail.com>
2022-06-06 12:26:53 +0300

    team already v2

--- cteam.php
+++ cteam.php
@@ -10,18 +10,19 @@ $rt = mysqli_num_rows($query);
 if($rt==1){
     header("Location: createteam.php?msg=tryagain");
 }
-$sql = "INSERT INTO team (name,numberPlayers)
-VALUES ('$teamname','$number')";
-mysqli_query($conn,$sql);
-$last_id = $conn->insert_id;
-$user=$_SESSION["id_user"];
-$_SESSION["id_team"] =$last_id;
-$sql1 = "INSERT INTO creates_team (id_user,id_team)
-VALUES ('$user','$last_id')";
-mysqli_query($conn,$sql1);
-$sql2 = "INSERT INTO is_member (id_user,id_team,role)
-VALUES ('$user','$last_id','leader')";
-mysqli_query($conn,$sql2);
-header("Location: homepage.php?msg=team");
-
+else{
+    $sql = "INSERT INTO team (name,numberPlayers)
+    VALUES ('$teamname','$number')";
+    mysqli_query($conn,$sql);
+    $last_id = $conn->insert_id;
+    $user=$_SESSION["id_user"];
+    $_SESSION["id_team"] =$last_id;
+    $sql1 = "INSERT INTO creates_team (id_user,id_team)
+    VALUES ('$user','$last_id')";
+    mysqli_query($conn,$sql1);
+    $sql2 = "INSERT INTO is_member (id_user,id_team,role)
+    VALUES ('$user','$last_id','leader')";
+    mysqli_query($conn,$sql2);
+    header("Location: homepage.php?msg=team");
+}
 ?>
