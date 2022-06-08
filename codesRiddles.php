35486bac9a3aea45d6985ff1be3b20f5f5ae84ce
categerjsy <katerinagerak99@gmail.com>
2022-06-08 17:41:58 +0300

    class try

--- codesRiddles.php
+++ codesRiddles.php
@@ -123,7 +123,7 @@ $thunt=$_SESSION["th"];
             <p>Please save this QRCode for your treasure hunt.</p>
             </div>
             <div class='col-75'>
-            <div class='qrcode' style='height:450px;width:450px;' v-loading='PanoramaInfo.bgenerateing'></div>
+            <div class='qrCode' data-qrcodeval='test qr code value 1'></div> 
             </div>
         </div>
             </div>
@@ -152,28 +152,88 @@ $thunt=$_SESSION["th"];
 
 window.onload = function ()
 {
-  console.log('onload');
-  let qrcode = new QRCode(document.getElementByClassName("qrcode")[0],
-             {
-              text: "https://arthunt.000webhostapp.com/participateTHunt.php?id="+"<?php echo $thunt ?>",
-              width: 450,
-              height: 450,
-              colorDark : "#000000",
-              colorLight : "#ffffff",
-              correctLevel : QRCode.CorrectLevel.H
-            });  
-  setTimeout(
-    function ()
-    {
-        let dataUrl = document.querySelector('#qrcode').querySelector('img').src;
-        downloadURI(dataUrl, 'qrcode.png');
-    }
-    ,1000);
+  $(".qrCode").each(function() {
+  $(this).qrcode({
+
+    // render method: 'canvas', 'image' or 'div'
+    render: 'div',
+
+    // version range somewhere in 1 .. 40
+    minVersion: 1,
+    maxVersion: 40,
+
+    // error correction level: 'L', 'M', 'Q' or 'H'
+    ecLevel: 'L',
+
+    // offset in pixel if drawn onto existing canvas
+    left: 0,
+    top: 0,
+
+    // size in pixel
+    size: 100,
+
+    // code color or image element
+    fill: '#fff',
+
+    // background color or image element, null for transparent background
+    background: null,
+
+    // content
+    text: $(this).data('qrcodeval'),
+
+    // corner radius relative to module width: 0.0 .. 0.5
+    radius: 0,
+
+    // quiet zone in modules
+    quiet: 0,
+
+    // modes
+    // 0: normal
+    // 1: label strip
+    // 2: label box
+    // 3: image strip
+    // 4: image box
+    mode: 0,
+
+    mSize: 0.1,
+    mPosX: 0.5,
+    mPosY: 0.5,
+
+    label: 'no label',
+    fontname: 'sans',
+    fontcolor: '#fff',
+
+    image: null
+  });
+});
+  // console.log('onload');
+  // var elements = document.getElementsByClassName("qrcode");
+  //   for (var i = 0; i < elements.length; i++) {
+  //       elements[i].style.paddingLeft = "50px";
+  //   }
+  // // let qrcode = new QRCode(document.getElementByClassName("qrcode")[0],
+  // //            {
+  // //             text: "https://arthunt.000webhostapp.com/participateTHunt.php?id="+"<?php echo $thunt ?>",
+  // //             width: 450,
+  // //             height: 450,
+  // //             colorDark : "#000000",
+  // //             colorLight : "#ffffff",
+  // //             correctLevel : QRCode.CorrectLevel.H
+  // //           });  
+  // setTimeout(
+  //   function ()
+  //   {
+  //       let dataUrl = document.querySelector('#qrcode').querySelector('img').src;
+  //       downloadURI(dataUrl, 'qrcode.png');
+  //   }
+  //   ,1000);
 
 };
     
   </script>
- 
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.qrcode/1.0/jquery.qrcode.min.js"></script>
+
   <script src="https://cdn.jsdelivr.net/gh/davidshimjs/qrcodejs/qrcode.min.js"></script>
   <script src="js/number.js"></script>
   <script src="js/disablePreviousDates.js"></script>
