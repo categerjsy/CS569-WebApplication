c86a5bf0d1fe0d8f08848606544e451075c27e87
categerjsy <katerinagerak99@gmail.com>
2022-06-08 14:34:27 +0300

    Correction

--- codesRiddles.php
+++ codesRiddles.php
@@ -64,74 +64,74 @@ $thunt=$_SESSION["th"];
     <section id="about">
     <div class='container'>
     <?php
-    $query = mysqli_query($conn, 'SELECT * FROM has WHERE id_thunt='$thunt'');
-    while ($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
-        $riddle=$row["id_riddle"];
-        $queryr = mysqli_query($conn, 'SELECT * FROM riddle WHERE id_riddle='$riddle'');
-        while ($rowr = mysqli_fetch_array($queryr, MYSQLI_ASSOC)) {
-        $text=$rowr['text'];
-        $location_solution=$rowr['location_solution'];
-        $object=$rowr['object_AR'];
-        $infotext=$rowr['infotext'];
-        $points=$rowr['points'];
-        echo "<div class='row'>
-          <div class='col-25'>
-            <p>Riddle's Text</p>
-          </div>
-          <div class='col-75'>
-            $text
-          </div>
-          </div>
-          <div class='row'>
-          <div class='col-25'>
-            <p>Riddle's solution</p>
-          </div>
-          <div class='col-75'>
-            $location_solution
-          </div>
-          </div>
-          <div class='row'>
-          <div class='col-25'>
-            <p>Riddle's Object</p>
-          </div>
-          <div class='col-75'>
-            $object
-          </div>
-          </div>
-          <div class='row'>
-          <div class='col-25'>
-            <p>Riddle's Information</p>
-          </div>
-          <div class='col-75'>
-            $infotext
-          </div>
-          </div>
-          <div class='row'>
-          <div class='col-25'>
-            <p>Riddle's point</p>
-          </div>
-          <div class='col-75'>
-            $points
-          </div>
-          </div>
-          <div class='row'>
-          <div class='col-25'>
-          </div>
-          <div class='col-75'>
-          <div class='row'>
-            <div class='col-25'>
-            <p>Please sent this QRCode to your team!</p>
-            </div>
-            <div class='col-75'>
-            <div id='qrcode' style='height:450px;width:450px;' v-loading='PanoramaInfo.bgenerateing'></div>
-            </div>
-        </div>
-            </div>
-        <div class='row'>
-        </div>";
-        }
-    }
-    ?>
+    // $query = mysqli_query($conn, 'SELECT * FROM has WHERE id_thunt='$thunt'');
+    // while ($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+    //     $riddle=$row["id_riddle"];
+    //     $queryr = mysqli_query($conn, 'SELECT * FROM riddle WHERE id_riddle='$riddle'');
+    //     while ($rowr = mysqli_fetch_array($queryr, MYSQLI_ASSOC)) {
+    //     $text=$rowr['text'];
+    //     $location_solution=$rowr['location_solution'];
+    //     $object=$rowr['object_AR'];
+    //     $infotext=$rowr['infotext'];
+    //     $points=$rowr['points'];
+    //     echo "<div class='row'>
+    //       <div class='col-25'>
+    //         <p>Riddle's Text</p>
+    //       </div>
+    //       <div class='col-75'>
+    //         $text
+    //       </div>
+    //       </div>
+    //       <div class='row'>
+    //       <div class='col-25'>
+    //         <p>Riddle's solution</p>
+    //       </div>
+    //       <div class='col-75'>
+    //         $location_solution
+    //       </div>
+    //       </div>
+    //       <div class='row'>
+    //       <div class='col-25'>
+    //         <p>Riddle's Object</p>
+    //       </div>
+    //       <div class='col-75'>
+    //         $object
+    //       </div>
+    //       </div>
+    //       <div class='row'>
+    //       <div class='col-25'>
+    //         <p>Riddle's Information</p>
+    //       </div>
+    //       <div class='col-75'>
+    //         $infotext
+    //       </div>
+    //       </div>
+    //       <div class='row'>
+    //       <div class='col-25'>
+    //         <p>Riddle's point</p>
+    //       </div>
+    //       <div class='col-75'>
+    //         $points
+    //       </div>
+    //       </div>
+    //       <div class='row'>
+    //       <div class='col-25'>
+    //       </div>
+    //       <div class='col-75'>
+    //       <div class='row'>
+    //         <div class='col-25'>
+    //         <p>Please sent this QRCode to your team!</p>
+    //         </div>
+    //         <div class='col-75'>
+    //         <div id='qrcode' style='height:450px;width:450px;' v-loading='PanoramaInfo.bgenerateing'></div>
+    //         </div>
+    //     </div>
+    //         </div>
+    //     <div class='row'>
+    //     </div>";
+    //     }
+    // }
+     ?>
     </div>
 
     </section>
