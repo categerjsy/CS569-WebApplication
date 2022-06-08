d779606e17e849d26dc6d82dc99deaa9ce622456
categerjsy <katerinagerak99@gmail.com>
2022-06-08 10:47:11 +0300

    solved

---
+++ Solved.php
@@ -0,0 +1,12 @@
+<?php
+
+include 'config.php';
+$id_riddle=$_GET["id_riddle"];
+//εδω καπως θα μπουν τα αντιστοιχα :3
+$id_thunt=5;
+$id_team=5;
+
+
+$sql = "INSERT INTO solve (id_riddle,id_thunt,id_team)
+VALUES ('$id_riddle','$id_thunt','$id_team')";
+mysqli_query($conn,$sql);
