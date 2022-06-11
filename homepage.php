554e5986557657a6e4ef292e946160c01bdf974d
categerjsy <katerinagerak99@gmail.com>
2022-06-11 19:26:04 +0300

    try the think for stackoverflow

--- homepage.php
+++ homepage.php
@@ -178,6 +178,7 @@ session_start ();
   </script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
-
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.js"></script>
+  
   
 </html>
