c65aa4628c2d5a209e23a922b0713afb4fd86583
categerjsy <katerinagerak99@gmail.com>
2022-06-11 13:48:11 +0300

    -1

--- ReturnThuntId.php
+++ ReturnThuntId.php
@@ -2,7 +2,7 @@
 include 'config.php';
 $thunt=$_POST["name"];
 
-
+$id_thunt=-1;
 $idQuery=mysqli_query($conn,"SELECT * FROM treasure_hunt WHERE name='$thunt'");
  while ($row = mysqli_fetch_array($idQuery, MYSQLI_ASSOC)) {
     $id_thunt=$row["id_thunt"];
