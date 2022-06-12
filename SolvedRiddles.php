978124e922ee203060ae870880c0bbabf0050483
categerjsy <katerinagerak99@gmail.com>
2022-06-12 13:12:33 +0300

    solced

--- SolvedRiddles.php
+++ SolvedRiddles.php
@@ -8,7 +8,7 @@ $checkQuery="SELECT * FROM has WHERE id_thunt='$id_thunt'";
 $result=mysqli_query($conn,$checkQuery);
 $total=mysqli_num_rows($result);
 
-$solveQuery="SELECT * FROM has WHERE id_thunt='$id_thunt' AND id_team='$id_team";
+$solveQuery="SELECT * FROM solved WHERE id_thunt='$id_thunt' AND id_team='$id_team";
 $solveResult=mysqli_query($conn,$solveQuery);
 $solved=mysqli_num_rows($solveResult);
 
