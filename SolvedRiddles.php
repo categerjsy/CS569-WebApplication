02b53a363172ef118f89178023c127666bcc6089
categerjsy <katerinagerak99@gmail.com>
2022-06-12 14:10:16 +0300

    solced

--- SolvedRiddles.php
+++ SolvedRiddles.php
@@ -4,11 +4,13 @@ include 'config.php';
 $id_team=5;
 $id_thunt=20;
 
+$total=-1;
 $checkQuery="SELECT * FROM has WHERE id_thunt='$id_thunt'";
 $result=mysqli_query($conn,$checkQuery);
 $total=mysqli_num_rows($result);
 
-$solveQuery="SELECT * FROM solve WHERE id_thunt='$id_thunt' AND id_team='$id_team";
+$solved=0;
+$solveQuery="SELECT * FROM solve WHERE id_thunt='$id_thunt' AND id_team='$id_team'";
 $solveResult=mysqli_query($conn,$solveQuery);
 $solved=mysqli_num_rows($solveResult);
 
