f5bd7579775bec831e22ffb407fe832dd6582760
categerjsy <katerinagerak99@gmail.com>
2022-06-14 17:28:25 +0300

    pngs post

--- RiddlesPng.php
+++ RiddlesPng.php
@@ -1,7 +1,7 @@
 <?php
 include 'config.php';
 
-$id_thunt=20;//$_POST["thunt"];
+$id_thunt=$_POST["thunt"];
 
 $query = mysqli_query($conn, "SELECT * FROM has WHERE id_thunt='$id_thunt'");
     while ($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
