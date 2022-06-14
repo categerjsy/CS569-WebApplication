c6a9f321dd1b847ddaa19abac46eb204a3f6fb3c
categerjsy <katerinagerak99@gmail.com>
2022-06-14 17:18:01 +0300

    pngs

--- RiddlesPng.php
+++ RiddlesPng.php
@@ -3,7 +3,7 @@ include 'config.php';
 
 $id_thunt=20;//$_POST["thunt"];
 
-query = mysqli_query($conn, "SELECT * FROM has WHERE id_thunt='$thunt'");
+$query = mysqli_query($conn, "SELECT * FROM has WHERE id_thunt='$thunt'");
     while ($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
         echo $row["qrcode"];
         echo "*";
