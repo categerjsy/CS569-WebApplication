a0ea1d0277ed7bcdbfddfd0a009e94679da8b796
categerjsy <katerinagerak99@gmail.com>
2022-06-17 18:40:56 +0300

    change

--- ReturnRiddleThunt.php
+++ ReturnRiddleThunt.php
@@ -2,10 +2,14 @@
 include 'config.php';
 session_start ();
 
-$thunt=$_POST["thunt"]; //εδω καλο ειναι να μπει Post αλλιως Get το βλέπουμε
+$name_thunt=$_POST["thunt"];
 
+$query = mysqli_query($conn, "SELECT * FROM treasure_hunt WHERE name='$name_thunt'");
+    while ($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+        $id_thunt=$row["id_thunt"];
+    }
 
-$query = mysqli_query($conn, "SELECT * FROM has WHERE id_thunt='$thunt'");
+$query = mysqli_query($conn, "SELECT * FROM has WHERE id_thunt='$id_thunt'");
     while ($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
         $riddle=$row["id_riddle"];
         $queryr = mysqli_query($conn, "SELECT * FROM riddle WHERE id_riddle='$riddle'");
