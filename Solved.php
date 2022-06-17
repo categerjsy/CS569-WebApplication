1d18984c68fe2732c4cea9f7114d4b623c900b6e
categerjsy <katerinagerak99@gmail.com>
2022-06-17 20:43:41 +0300

    dada

--- Solved.php
+++ Solved.php
@@ -4,10 +4,12 @@ include 'config.php';
 $id_riddle=$_GET["r"];
 //εδω καπως θα μπουν τα αντιστοιχα :3
 $id_thunt=$_POST["id_thunt"];
-$id_team=$_POST["id_team"];
+$team=$_POST["team"];
 
-
-
+$idQuery=mysqli_query($conn,"SELECT * FROM team WHERE name='$team'");
+ while ($row = mysqli_fetch_array($idQuery, MYSQLI_ASSOC)) {
+    $id_team=$row["id_team"];
+ }
 
 $checkQuery="SELECT * FROM solve WHERE id_riddle='$id_riddle' AND id_thunt='$id_thunt' AND id_team='$id_team'";
 $result=mysqli_query($conn,$checkQuery);
