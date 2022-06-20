0d8c07c018313a7b22b9f5deae0348b1ef1f1d51
categerjsy <katerinagerak99@gmail.com>
2022-06-20 12:08:53 +0300

    cor

--- codesRiddles.php
+++ codesRiddles.php
@@ -137,9 +137,9 @@ $tempDir = "qrcodes/";
                     QRcode::png($link, $pngAbsoluteFilePath, QR_ECLEVEL_H, 10);
                 } 
               echo '<div class="row"><div class="col-25">';
-              echo '<img id="'.$number.'" src="'.$urlRelativeFilePath.'" />';
+              echo '<img width='100' height='100' id="'.$number.'" src="'.$urlRelativeFilePath.'" />';
               echo "</div><div class='col-75'>";
-              echo "<p><a href='https://arthunt.000webhostapp.com/download.php?path=$urlRelativeFilePath' ><img  src='img/download-svgrepo-com.svg'  width='5' height='5'/>QRCode riddle</a></p>";
+              echo "<p><a href='https://arthunt.000webhostapp.com/download.php?path=$urlRelativeFilePath' ><img  src='img/download-svgrepo-com.svg'  width='25' height='25'/>QRCode riddle</a></p>";
               $sqli = "UPDATE has SET qrcode='$urlRelativeFilePath' WHERE id_thunt='$thunt' AND id_riddle='$riddle'";
               mysqli_query($conn,$sqli);
        echo 
