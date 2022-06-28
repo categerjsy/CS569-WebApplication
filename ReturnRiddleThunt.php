ba267e1caa54044e7333c85072ba8d274b417edb
categerjsy <katerinagerak99@gmail.com>
2022-06-28 14:17:08 +0300

    corr

--- ReturnRiddleThunt.php
+++ ReturnRiddleThunt.php
@@ -2,7 +2,7 @@
 include 'config.php';
 session_start ();
 
-$name_thunt=$_POST["thunt"];
+$name_thunt="Demo 2";//$_POST["thunt"];
 
 $query = mysqli_query($conn, "SELECT * FROM treasure_hunt WHERE name='$name_thunt'");
     while ($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
